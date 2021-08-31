45c0d54d33a103681de9e44b1a7eec505866ab30
Alef <83234291+fullstackwd@users.noreply.github.com>
2021-08-31 18:30:30 -0300

    Update 5-main.c
    **

--- 0x06-pointers_arrays_strings/5-main.c
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -8,10 +8,11 @@
  */
 int main(void)
 {
+    char string_toupper;
     char str[] = "Holberton School!\n";
     char *ptr;
 
-    ptr = *string_toupper(str);
+    ptr = string_toupper(str);
     printf("%s", ptr);
     printf("%s", str);
     return (0);
